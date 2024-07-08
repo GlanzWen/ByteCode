d41357c97045273a7bf4520172c9740c8b4c84e3
GlanzWen <281616236@qq.com>
2024-07-08 16:48:45 +0800

    init push

---
+++ src/main/java/com/glanz/bytecodes/BytecodesApplication.java
@@ -0,0 +1,13 @@
+package com.glanz.bytecodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytecodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BytecodesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glanz/bytecodes/BytecodesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glanz.bytecodes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytecodesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
